25d0611ba548938efd0e5ae719785f12f2051f90
gio797 <g.muchaidze@yahoo.com>
2023-09-27 14:45:55 +0400

    add mongoose

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
+import Deck from "./models/Deck.js";
+import bodyParser from "body-parser";
+
+const PORT = 5000;
 
 const app = express();
-app.get("/", (req, res) => {
-  res.json("hello");
+app.use(bodyParser.json());
+
+app.get("/decks", async (req, res) => {
+  const decks = await Deck.find({});
+  res.json(decks);
 });
 
-app.listen(3000, console.log("listening"));
+app.post("/decks", async (req, res) => {
+  const data = req.body;
+  const newDeck = new Deck(data);
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://giomuchaidze:chelsea123@cards-claster.yok3xyg.mongodb.net/"
+  )
+  .then(() => {
+    app.listen(PORT, console.log("listening"));
+  });
